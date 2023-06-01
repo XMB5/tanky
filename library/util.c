65941984fea2e907cb8facbef7d2b88f819973f6
sfoxman@caltech.edu <sfoxman@caltech.edu>
2023-06-01 08:16:01 +0000

    Fix pointer type warning

--- library/util.c
+++ library/util.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include <stdlib.h>
-#include <util.h>
 #include <string.h>
+#include <util.h>
 
 const double PI = 3.1415926535897932384626433832795;
 
@@ -24,6 +24,6 @@ double rand_range(double min, double max) {
 const char *strdup_safe(const char *str) {
   size_t str_len = strlen(str);
   const char *new = malloc_safe(str_len + 1);
-  memcpy(new, str, str_len + 1);
+  memcpy((void *)new, str, str_len + 1);
   return new;
 }
